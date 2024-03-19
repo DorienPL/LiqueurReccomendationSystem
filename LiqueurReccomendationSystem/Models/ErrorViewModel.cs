2bf4b304f0006e22a8215ed5ddf3b80e83f7443a
DorienPL <dorienpl@gmail.com>
2024-03-19 13:22:34 +0100

    Initial commit

---
+++ LiqueurReccomendationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LiqueurReccomendationSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
